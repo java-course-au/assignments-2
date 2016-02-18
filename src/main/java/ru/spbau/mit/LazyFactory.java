407b96836110c4eb52a41497cc9b36978cf350d5
LabutinIgor <Labutin.IgorL@gmail.com>
2016-02-19 01:52:41 +0300

    Fixed LazyFactory

--- src/main/java/ru/spbau/mit/LazyFactory.java
+++ src/main/java/ru/spbau/mit/LazyFactory.java
@@ -1,51 +1,88 @@
 package ru.spbau.mit;
 
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 import java.util.function.Supplier;
 
 public class LazyFactory {
-    public static <T> Lazy<T> createLazyOneThread(final Supplier<T> supplier) {
-        return new Lazy<T>() {
-            private T result;
-            private boolean calculated = false;
+    private static class OneThreadLazy<T> implements Lazy<T> {
+        private static final Object NONE = new Object();
+        private Supplier<T> supplier;
+        private Object result = NONE;
 
-            public T get() {
-                if (!calculated) {
-                    result = supplier.get();
-                    calculated = true;
-                }
-                return result;
+        OneThreadLazy(Supplier<T> supplier) {
+            this.supplier = supplier;
+        }
+
+        @Override
+        public T get() {
+            if (result == NONE) {
+                result = supplier.get();
+                supplier = null;
             }
-        };
+            return (T) result;
+        }
+    }
+
+    private static class MultiThreadLazy<T> implements Lazy<T> {
+        private static final Object NONE = new Object();
+        private Supplier<T> supplier;
+        private volatile Object result = NONE;
+
+        MultiThreadLazy(Supplier<T> supplier) {
+            this.supplier = supplier;
+        }
+
+        @Override
+        public T get() {
+            if (result == NONE) {
+                synchronized (this) {
+                    if (result == NONE) {
+                        result = supplier.get();
+                        supplier = null;
+                    }
+                }
+            }
+            return (T) result;
+        }
+    }
+
+    private static class LockFreeLazy<T> implements Lazy<T> {
+        private static final Object NONE = new Object();
+        private Supplier<T> supplier;
+        private final static
+        AtomicReferenceFieldUpdater<LockFreeLazy, Object> updater =
+                AtomicReferenceFieldUpdater.newUpdater(
+                        LockFreeLazy.class, Object.class, "result");
+        private volatile Object result = NONE;
+
+        LockFreeLazy(Supplier<T> supplier) {
+            this.supplier = supplier;
+        }
+
+        @Override
+        public T get() {
+            if (result == NONE) {
+                Supplier<T> local = supplier;
+                if (local != null) {
+                    if (updater.compareAndSet(this, NONE, local.get())) {
+                        supplier = null;
+                    }
+                }
+            }
+            return (T) result;
+        }
+    }
+
+    public static <T> Lazy<T> createLazyOneThread(final Supplier<T> supplier) {
+        return new OneThreadLazy<>(supplier);
     }
 
     public static <T> Lazy<T> createLazyMultiThread(final Supplier<T> supplier) {
-        return new Lazy<T>() {
-            private T result;
-            private boolean calculated = false;
-
-            public synchronized T get() {
-                if (!calculated) {
-                    result = supplier.get();
-                    calculated = true;
-                }
-                return result;
-            }
-        };
+        return new MultiThreadLazy<>(supplier);
     }
 
     public static <T> Lazy<T> createLazyLockFree(final Supplier<T> supplier) {
-        return new Lazy<T>() {
-            private AtomicReference<T> result = new AtomicReference<>(null);
-            private volatile boolean calculated = false;
-
-            public T get() {
-                if (!calculated) {
-                    result.compareAndSet(null, supplier.get());
-                    calculated = true;
-                }
-                return result.get();
-            }
-        };
+        return new LockFreeLazy<>(supplier);
     }
 }
