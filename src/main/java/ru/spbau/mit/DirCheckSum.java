4fda3bce2ddbbfaeb511f3d656b9087a65eacb8b
LizaTretyakova <elizabet.tretyakova@gmail.com>
2016-03-22 01:57:13 +0300

    first of three parts

--- src/main/java/ru/spbau/mit/DirCheckSum.java
+++ src/main/java/ru/spbau/mit/DirCheckSum.java
@@ -1,23 +1,23 @@
 package ru.spbau.mit;
 
-import javafx.scene.shape.Path;
+import org.apache.commons.codec.digest.DigestUtils;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.RecursiveTask;
 
-/**
- * Created by liza on 01.03.16.
- */
 public class DirCheckSum {
 
     public interface CheckSum<T> {
 
-        String checkSum(String dirname);
+        int BUFFER_SIZE = 4096;
+
+        String checkSum(Path dirname) throws NoSuchAlgorithmException, FileNotFoundException;
 
         static String toMD5(String path) throws IOException {
             FileInputStream fis = new FileInputStream(new File("foo"));
@@ -30,8 +30,28 @@ public class DirCheckSum {
     public class CheckSumOneThread implements CheckSum {
 
         @Override
-        public String checkSum(String path) {
-            if(Files.isDirectory())
+        public String checkSum(Path path) throws NoSuchAlgorithmException, FileNotFoundException {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+
+            if(Files.isDirectory(path)) {
+                md5.update(path.toString().getBytes());
+                for(File fileEntry: path.toFile().listFiles()) {
+                    md5.update(checkSum(fileEntry.toPath()).getBytes());
+                }
+            } else {
+                DigestInputStream in = new DigestInputStream(
+                        new FileInputStream(path.toFile()),
+                        md5);
+                in.on(true);
+                byte[] tmp = new byte[BUFFER_SIZE];
+                try {
+                    while(true) {
+                        in.read(tmp, 0, BUFFER_SIZE);
+                    }
+                } catch (IOException e) {}
+            }
+
+            return new String(md5.digest());
         }
     }
 
@@ -45,7 +65,8 @@ public class DirCheckSum {
 }
 
 /*
-Есть объект. У него есть метод accept. Он спит, пока кто-то не подключится. При подключении выпл1вывает сокет, который обрабатывает в другом потоке
+Есть объект. У него есть метод accept. Он спит, пока кто-то не подключится. При подключении выплёвывает сокет,
+который обрабатывает в другом потоке.
 
 Плохой синтаксис, но всё же
 while(s = r.accept != null) {
