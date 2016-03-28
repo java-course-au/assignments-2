f449cde775e24ee2f030ad4ddfc00e18a9d9ccfe
Yurii Rebryk <y.a.rebryk@gmail.com>
2016-03-29 01:30:09 +0300

    Solution modified.

--- src/test/java/ru/spbau/Tests.java
+++ src/test/java/ru/spbau/Tests.java
@@ -1,36 +1,54 @@
 package ru.spbau;
 
-import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
+
+/*
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-//import java.util.List;
-import static org.junit.Assert.assertArrayEquals;
+import java.util.ArrayList;
+import org.apache.commons.io.IOUtils;
+import static org.junit.Assert.assertArrayEquals;*/
 
 /**
  * Created by rebryk on 10/03/16.
  */
 public class Tests {
-    public static final int PORT = 5000;
+    public static final int PORT = 1024;
     public static final String HOSTNAME = "localhost";
 
-    //public static final String DIR_PATH = "src/test/resources/test";
+    public static final String DIR_PATH = "src/test/resources/test";
     public static final String FILE_PATH = "src/test/resources/test/1.png";
 
+    //public static final int CLIENTS_COUNT = 5;
+
     @Test
-    public void testFtp() throws IOException {
+    public void testGetListSingleThread() throws IOException {
         FtpServer server = new FtpServer(PORT);
         FtpClient client = new FtpClient(HOSTNAME, PORT);
 
         server.start();
         client.start();
 
-        //List<String> list = client.getList(DIR_PATH);
-        //Assert.assertNotNull(list);
+        List<String> list = client.getList(DIR_PATH);
+        Assert.assertNotNull(list);
+
+        server.stop();
+        client.stop();
+    }
+
+    /*
+    @Test
+    public void testGetFileSingleThread() throws IOException {
+        FtpServer server = new FtpServer(PORT);
+        FtpClient client = new FtpClient(HOSTNAME, PORT);
+
+        server.start();
+        client.start();
 
         InputStream file = Files.newInputStream(Paths.get(FILE_PATH));
         InputStream data = client.getFile(FILE_PATH);
@@ -41,4 +59,40 @@ public class Tests {
         server.stop();
         client.stop();
     }
+
+    @Test
+    public void testGetListMultiThread() {
+        FtpServer server = new FtpServer(PORT);
+
+        server.start();
+
+        List<Thread> clients = new ArrayList<>();
+        for (int i = 0; i < CLIENTS_COUNT; ++i) {
+            clients.add(new Thread(() -> {
+                    FtpClient client = new FtpClient(HOSTNAME, PORT);
+                    client.start();
+
+                    List<String> list = null;
+                    try {
+                        list = client.getList(DIR_PATH);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Assert.assertNotNull(list);
+                    client.stop();
+                }));
+        }
+
+        clients.forEach(Thread::start);
+
+        try {
+            for (Thread thread: clients) {
+                thread.join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        server.stop();
+    }*/
 }
