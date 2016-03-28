8332409392f3f7183a45b2c47901d1ddf62cd4fa
Yurii Rebryk <y.a.rebryk@gmail.com>
2016-03-29 01:20:52 +0300

    Solution modified.

--- src/test/java/ru/spbau/Tests.java
+++ src/test/java/ru/spbau/Tests.java
@@ -8,17 +8,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
+//import java.util.List;
 import static org.junit.Assert.assertArrayEquals;
 
 /**
  * Created by rebryk on 10/03/16.
  */
 public class Tests {
-    public static final int PORT = 1024;
+    public static final int PORT = 5000;
     public static final String HOSTNAME = "localhost";
 
-    public static final String DIR_PATH = "src/test/resources/test";
+    //public static final String DIR_PATH = "src/test/resources/test";
     public static final String FILE_PATH = "src/test/resources/test/1.png";
 
     @Test
@@ -29,8 +29,8 @@ public class Tests {
         server.start();
         client.start();
 
-        List<String> list = client.getList(DIR_PATH);
-        Assert.assertNotNull(list);
+        //List<String> list = client.getList(DIR_PATH);
+        //Assert.assertNotNull(list);
 
         InputStream file = Files.newInputStream(Paths.get(FILE_PATH));
         InputStream data = client.getFile(FILE_PATH);
