6fc7b4f45833c86e34c96456d755cf92377fca0f
Yurii Rebryk <y.a.rebryk@gmail.com>
2016-04-12 13:18:29 +0300

    Convex hull implemented.

--- src/main/java/ru/spbau/mit/Main.java
+++ src/main/java/ru/spbau/mit/Main.java
@@ -26,6 +26,22 @@ public final class Main {
         // Return JMenuBar with one JMenu called "Main"
         // This JMenu should contain "Calculate" and "Clear" JMenuItems which call same methods in Canvas
 
-        throw new UnsupportedOperationException();
+        JMenu menu = new JMenu("Main");
+        JMenuItem itemCalculate = new JMenuItem("Calculate");
+        itemCalculate.addActionListener((event) -> {
+            canvas.calculate();
+        });
+        menu.add(itemCalculate);
+
+        JMenuItem itemClear = new JMenuItem("Clear");
+        itemClear.addActionListener((event) -> {
+            canvas.clear();
+        });
+        menu.add(itemClear);
+
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.add(menu);
+
+        return menuBar;
     }
 }
