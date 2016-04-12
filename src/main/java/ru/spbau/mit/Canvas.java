2ec3c351c5dec6f559559ba903d1a21bc82de64a
LDVSOFT <lapshin.dv@mail.ru>
2016-04-12 13:49:50 +0300

    Done

--- src/main/java/ru/spbau/mit/Canvas.java
+++ src/main/java/ru/spbau/mit/Canvas.java
@@ -1,36 +1,97 @@
 package ru.spbau.mit;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class Canvas extends JPanel implements DefaultMouseListener {
+    private static final int RADIUS = 10;
+    private final MyPopupMenu popupMenu = new MyPopupMenu();
 
-    private final JPopupMenu popupMenu = new JPopupMenu();
+    private final List<Point> points = new ArrayList<>();
+    private List<Point> convexHull = Collections.emptyList();
 
     Canvas() {
         addMouseListener(this);
         popupMenu.add(buildPopupMenuItem());
+        this.setComponentPopupMenu(popupMenu);
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
         switch (e.getButton()) {
             case MouseEvent.BUTTON1:
-                throw new UnsupportedOperationException();
-            case MouseEvent.BUTTON3:
-                throw new UnsupportedOperationException();
-            default:
-                throw new UnsupportedOperationException();
+                if (popupMenu.isVisible()) {
+                    return;
+                }
+                points.add(new Point(e.getPoint()));
+                repaint();
+                break;
         }
     }
 
     public void calculate() {
-        throw new UnsupportedOperationException();
+        if (points.size() <= 1) {
+            convexHull = Collections.emptyList();
+            return;
+        }
+        int basePointId = 0;
+        for (int i = 1; i != points.size(); i++) {
+            Point basePoint = points.get(basePointId);
+            Point altPoint = points.get(i);
+            if (Point.BASE_POINT_COMPARATOR.compare(basePoint, altPoint) != 1) {
+                continue;
+            }
+            basePointId = i;
+        }
+        Point basePoint = points.get(basePointId);
+
+        List<Point> candidates = new ArrayList<>(points.size());
+        for (int i = 0; i != points.size(); i++) {
+            if (i == basePointId) {
+                continue;
+            }
+            candidates.add(points.get(i).subtract(basePoint));
+        }
+        candidates.sort(Point.ANGLE_COMPARATOR);
+        candidates.add(0, Point.ZERO);
+
+        convexHull = new ArrayList<>();
+        for (Point candidate: candidates) {
+            while (convexHull.size() >= 2) {
+                Point b = convexHull.get(convexHull.size() - 1);
+                Point c = convexHull.get(convexHull.size() - 2);
+                Point ba = candidate.subtract(b);
+                Point cb = b.subtract(c);
+                if (cb.cross(ba) > 0) {
+                    break;
+                }
+                convexHull.remove(convexHull.size() - 1);
+            }
+            convexHull.add(candidate);
+        }
+        convexHull.add(Point.ZERO);
+
+        convexHull = convexHull
+                .stream()
+                .map(point -> point.add(basePoint))
+                .collect(Collectors.toList());
+        repaint();
     }
 
     public void clear() {
-        throw new UnsupportedOperationException();
+        points.clear();
+        convexHull.clear();
+        repaint();
     }
 
     @Override
@@ -38,15 +99,43 @@ class Canvas extends JPanel implements DefaultMouseListener {
         // To execute this code call repaint method
 
         g.clearRect(0, 0, getWidth(), getHeight());
-        g.setColor(Color.BLACK);
+        ((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g.setColor(Color.CYAN);
+        for (Point point: points) {
+            g.fillOval(point.getX() - RADIUS + 1, point.getY() - RADIUS + 1, 2 * RADIUS - 1, 2 * RADIUS - 1);
+        }
+        g.setColor(Color.BLUE);
+        for (Point point: points) {
+            g.drawOval(point.getX() - RADIUS, point.getY() - RADIUS, 2 * RADIUS, 2 * RADIUS);
+        }
+        for (int i = 0; i < convexHull.size() - 1; i++) {
+            Point a = convexHull.get(i);
+            Point b = convexHull.get(i + 1);
+            Point offset = b.subtract(a).norm().setLength(RADIUS + 1);
+            Point a1 = a.add(offset);
+            Point b1 = b.add(offset);
+            g.drawLine(a1.getX(), a1.getY(), b1.getX(), b1.getY());
+        }
 
-        throw new UnsupportedOperationException();
     }
 
     private JMenuItem buildPopupMenuItem() {
         // Return JMenuItem called "Remove point"
         // Point should be removed after click
-
-        throw new UnsupportedOperationException();
+        JMenuItem remove = new JMenuItem("Remove point");
+        remove.addActionListener(e -> {
+            Point p = null;
+            Point clickPosition = popupMenu.getLastPoint();
+            for (Point point: points) {
+                if (p == null || point.distance(clickPosition) < p.distance(clickPosition)) {
+                    p = point;
+                }
+            }
+            if (p != null) {
+                points.remove(p);
+                repaint();
+            }
+        });
+        return remove;
     }
 }
