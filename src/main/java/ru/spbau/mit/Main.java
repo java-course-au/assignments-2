b4a8e864cba0cf999da0848d080a90788b639e5e
Olga Chernikova <Oche24912@yandex.ru>
2016-06-01 08:13:03 +0300

    write more info

--- src/main/java/ru/spbau/mit/Main.java
+++ src/main/java/ru/spbau/mit/Main.java
@@ -6,19 +6,20 @@ public final class Main {
     private Main(){}
 
     public static void main(String[] args) {
+        System.out.println("File: " + args[0]);
         long timeBegin = System.currentTimeMillis();
         String sum = MD5Sum.countMD5SumInOneThread(new File(args[0]));
-        System.out.println("MD5 Sum:" + sum
+        System.out.println("In One thread MD5 Sum:" + sum
                 + "\ntime in one thread in mills: " + (System.currentTimeMillis() - timeBegin));
 
         timeBegin = System.currentTimeMillis();
         sum = MD5Sum.countMD5SumInExecutorService(new File(args[0]));
-        System.out.println("MD5 Sum:" + sum
+        System.out.println("Executor Service MD5 Sum:" + sum
                 + "\ntime in one thread in mills: " + (System.currentTimeMillis() - timeBegin));
 
         timeBegin = System.currentTimeMillis();
         sum = MD5Sum.countMD5SumInForkJoin(new File(args[0]));
-        System.out.println("MD5 Sum:" + sum
+        System.out.println("Fork Join MD5 Sum:" + sum
                 + "\ntime in one thread in mills: " + (System.currentTimeMillis() - timeBegin));
 
     }
