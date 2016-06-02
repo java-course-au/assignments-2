e7b6735cc503796224f8e96c1e3a8ab6c23a30bd
LabutinIgor <Labutin.IgorL@gmail.com>
2016-06-02 16:48:05 +0300

    Made task

--- src/test/java/ru/spbau/mit/Test.java
+++
@@ -1,5 +0,0 @@
-package ru.spbau.mit;
-
-public class Test {
-
-}
--- src/test/java/ru/spbau/mit/ThreadExpectedException.java
+++ src/test/java/ru/spbau/mit/ThreadExpectedException.java
@@ -4,18 +4,41 @@ import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ThreadExpectedException implements TestRule {
+    private Class<? extends Throwable> expectedException = null;
+    private Map<Thread, Throwable> threads = new HashMap<>();
 
     @Override
     public Statement apply(Statement base, Description description) {
-        return null;
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                base.evaluate();
+                for (Map.Entry<Thread, Throwable> entry : threads.entrySet()) {
+                    Thread thread = entry.getKey();
+                    Throwable throwable = entry.getValue();
+                    if (thread.getState() != Thread.State.TERMINATED) {
+                        throw new Exception("Thread " + thread.getName() + " is not terminated");
+                    }
+                    if ((expectedException == null && throwable != null)
+                            || (expectedException != null
+                            && (throwable == null || !throwable.getClass().isAssignableFrom(expectedException)))) {
+                        throw new Exception("Unexpected exception in thread " + thread.getName());
+                    }
+                }
+            }
+        };
     }
 
     public void expect(Class<? extends Throwable> e) {
-        
+        expectedException = e;
     }
 
-    public void registerThread(Thread t) {
-
+    public void registerThread(Thread thread) {
+        threads.put(thread, null);
+        thread.setUncaughtExceptionHandler(threads::replace);
     }
 }
